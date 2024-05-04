692ef11d4aece93586cfce3b70f19850afb47c1c
Brandon <brandon101morgan@outlook.com>
2024-05-04 17:05:42 -0500

    Hidden

--- Django Backend/Entrusted Grace Ministries/Entrusted_Grace_Ministries/settings.py
+++ Django Backend/Entrusted Grace Ministries/Entrusted_Grace_Ministries/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f1a41d54-a810-4114-b279-bf6200535cfd'
+SECRET_KEY = 'Shhhh!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
